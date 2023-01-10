3fef67346e991fafed4a48831c59a8026c05c48a
Ostabo <osi.borkenhagen@gmail.com>
2023-01-10 16:51:00 +0100

    adjust packages, ssl/https and port

--- vite.config.js
+++ vite.config.js
@@ -1,15 +1,23 @@
-import { fileURLToPath, URL } from 'node:url'
+import {fileURLToPath, URL} from 'node:url'
 
-import { defineConfig } from 'vite'
+import {defineConfig} from 'vite'
 import vue from '@vitejs/plugin-vue'
+import basicSsl from "@vitejs/plugin-basic-ssl"
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
-  resolve: {
-    alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
-    }
-  },
-  base: '/creative-coding-ws-22-23/'
+    plugins: [
+        basicSsl(),
+        vue()
+    ],
+    resolve: {
+        alias: {
+            '@': fileURLToPath(new URL('./src', import.meta.url))
+        }
+    },
+    server: {
+        port: 8080,
+        https: true,
+    },
+    base: '/creative-coding-ws-22-23/'
 })
