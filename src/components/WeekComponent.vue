88a8991d6cc87055b9226ac8940d182ac90dc8e9
Ostabo <osi.borkenhagen@gmail.com>
2023-02-09 16:17:46 +0100

    implement lazy loading for weekly page

--- src/components/WeekComponent.vue
+++ src/components/WeekComponent.vue
@@ -1,110 +1,117 @@
 <template>
-  <v-responsive max-width="1550px">
-    <h1 class="text-left em-buffer-top">Woche {{ week }}</h1>
-    <div class="text-left em-buffer-bot">{{ json.description }}</div>
-    <v-row v-for="person in json?.people?.filter(p => hasSomething(p))"
-           :key="person"
-           :align="'center'"
-           :justify="'center'"
-           class="my-5">
-      <div class="text-disabled w-100 text-start ms-4">{{ person.name }}</div>
-      <v-card v-for="img in person.img" class="ma-1" elevation="0" height="220" min-width="300" rounded="0" width="380"
-              @click="overlay[img] = true">
-        <v-icon v-if="img.toString().endsWith('.gif')"
-                :icon="'mdi-file-gif-box'"
-                :size="220"
-                class="text-center">
-        </v-icon>
-        <v-img v-else
-               :cover="true"
-               :lazy-src="'https://via.placeholder.com/380x220.webp?text=Loading...'"
-               :src="getImg(img)"
-               height="220px"
-               width="380px"
-        >
-        </v-img>
-      </v-card>
-
-      <v-card v-for="ifr in person.iframe" class="ma-1" elevation="0" min-width="300" rounded="0"
-              @click="overlay[ifr.src] = true">
-        <v-img
-            :cover="true"
-            :lazy-src="'https://via.placeholder.com/380x220.webp?text=Loading...'"
-            :src="getImg(ifr.thumbnail || 'p5js.png')"
-            height="220px"
-            width="380px"
-        >
-        </v-img>
-      </v-card>
-
-      <v-card v-for="pdf in person.pdf" class="ma-1" elevation="0" height="220" min-width="300" rounded="0" width="380"
-              @click="overlay[pdf] = true">
-        <v-icon :icon="'mdi-file-pdf-box'"
-                :size="220"
-                class="text-center">
-        </v-icon>
-      </v-card>
-
-      <v-dialog v-for="ifr in person.iframe" v-model="overlay[ifr.src]" :scrollable="true"
-                :width="$vuetify.display.xs || $vuetify.display.sm ? '' : '60%'">
-        <v-card>
-          <v-toolbar>
-            <v-btn :href="ifr.src" icon target="_blank">
-              <v-icon :icon="'mdi-open-in-new'"></v-icon>
-            </v-btn>
-            <v-spacer></v-spacer>
-            <v-btn icon @click="overlay[ifr.src] = false">
-              <v-icon :icon="'mdi-close'"></v-icon>
-            </v-btn>
-          </v-toolbar>
-          <iframe
-              :allowfullscreen="'true'"
-              :height="800"
-              :src="ifr.src"
-              :width="'100%'"
-              allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share; camera; microphone"
-              class="border"
-          ></iframe>
-        </v-card>
-      </v-dialog>
-
-      <v-dialog v-for="img in person.img" v-model="overlay[img]" :scrollable="true"
-                :width="$vuetify.display.xs || $vuetify.display.sm ? '' : '60%'">
-        <v-card>
-          <v-toolbar>
-            <v-spacer></v-spacer>
-            <v-btn icon @click="overlay[img] = false">
-              <v-icon :icon="'mdi-close'"></v-icon>
-            </v-btn>
-          </v-toolbar>
-          <v-img
-              :lazy-src="'https://via.placeholder.com/380x220.webp?text=Loading...'"
-              :src="getImg(img)"
+  <v-lazy v-model="active"
+          :options="{threshold: .1}"
+          min-height="100vh"
+          transition="fade-transition">
+    <v-responsive max-width="1550px">
+      <h1 class="text-left em-buffer-top">Woche {{ week }}</h1>
+      <div class="text-left em-buffer-bot">{{ json.description }}</div>
+      <v-row v-for="person in json?.people?.filter(p => hasSomething(p))"
+             :key="person"
+             :align="'center'"
+             :justify="'center'"
+             class="my-5">
+        <div class="text-disabled w-100 text-start ms-4">{{ person.name }}</div>
+        <v-card v-for="img in person.img" class="ma-1" elevation="0" height="220" min-width="300" rounded="0"
+                width="380"
+                @click="overlay[img] = true">
+          <v-icon v-if="img.toString().endsWith('.gif')"
+                  :icon="'mdi-file-gif-box'"
+                  :size="220"
+                  class="text-center">
+          </v-icon>
+          <v-img v-else
+                 :cover="true"
+                 :lazy-src="'https://via.placeholder.com/380x220.webp?text=Loading...'"
+                 :src="getImg(img)"
+                 height="220px"
+                 width="380px"
           >
           </v-img>
         </v-card>
-      </v-dialog>
 
-      <v-dialog v-for="pdf in person.pdf" v-model="overlay[pdf]" :scrollable="true"
-                :width="$vuetify.display.xs || $vuetify.display.sm ? '' : '60%'">
-        <v-card>
-          <v-toolbar>
-            <v-btn :href="getPdf(pdf)"
-                   download
-                   icon>
-              <v-icon :icon="'mdi-download'"></v-icon>
-            </v-btn>
-            <v-spacer></v-spacer>
-            <v-btn icon @click="overlay[pdf] = false">
-              <v-icon :icon="'mdi-close'"></v-icon>
-            </v-btn>
-          </v-toolbar>
-          <VuePdfEmbed :source="getPdf(pdf)"/>
+        <v-card v-for="ifr in person.iframe" class="ma-1" elevation="0" min-width="300" rounded="0"
+                @click="overlay[ifr.src] = true">
+          <v-img
+              :cover="true"
+              :lazy-src="'https://via.placeholder.com/380x220.webp?text=Loading...'"
+              :src="getImg(ifr.thumbnail || 'p5js.png')"
+              height="220px"
+              width="380px"
+          >
+          </v-img>
         </v-card>
-      </v-dialog>
 
-    </v-row>
-  </v-responsive>
+        <v-card v-for="pdf in person.pdf" class="ma-1" elevation="0" height="220" min-width="300" rounded="0"
+                width="380"
+                @click="overlay[pdf] = true">
+          <v-icon :icon="'mdi-file-pdf-box'"
+                  :size="220"
+                  class="text-center">
+          </v-icon>
+        </v-card>
+
+        <v-dialog v-for="ifr in person.iframe" v-model="overlay[ifr.src]" :scrollable="true"
+                  :width="$vuetify.display.xs || $vuetify.display.sm ? '' : '60%'">
+          <v-card>
+            <v-toolbar>
+              <v-btn :href="ifr.src" icon target="_blank">
+                <v-icon :icon="'mdi-open-in-new'"></v-icon>
+              </v-btn>
+              <v-spacer></v-spacer>
+              <v-btn icon @click="overlay[ifr.src] = false">
+                <v-icon :icon="'mdi-close'"></v-icon>
+              </v-btn>
+            </v-toolbar>
+            <iframe
+                :allowfullscreen="'true'"
+                :height="800"
+                :src="ifr.src"
+                :width="'100%'"
+                allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share; camera; microphone"
+                class="border"
+            ></iframe>
+          </v-card>
+        </v-dialog>
+
+        <v-dialog v-for="img in person.img" v-model="overlay[img]" :scrollable="true"
+                  :width="$vuetify.display.xs || $vuetify.display.sm ? '' : '60%'">
+          <v-card>
+            <v-toolbar>
+              <v-spacer></v-spacer>
+              <v-btn icon @click="overlay[img] = false">
+                <v-icon :icon="'mdi-close'"></v-icon>
+              </v-btn>
+            </v-toolbar>
+            <v-img
+                :lazy-src="'https://via.placeholder.com/380x220.webp?text=Loading...'"
+                :src="getImg(img)"
+            >
+            </v-img>
+          </v-card>
+        </v-dialog>
+
+        <v-dialog v-for="pdf in person.pdf" v-model="overlay[pdf]" :scrollable="true"
+                  :width="$vuetify.display.xs || $vuetify.display.sm ? '' : '60%'">
+          <v-card>
+            <v-toolbar>
+              <v-btn :href="getPdf(pdf)"
+                     download
+                     icon>
+                <v-icon :icon="'mdi-download'"></v-icon>
+              </v-btn>
+              <v-spacer></v-spacer>
+              <v-btn icon @click="overlay[pdf] = false">
+                <v-icon :icon="'mdi-close'"></v-icon>
+              </v-btn>
+            </v-toolbar>
+            <VuePdfEmbed :source="getPdf(pdf)"/>
+          </v-card>
+        </v-dialog>
+
+      </v-row>
+    </v-responsive>
+  </v-lazy>
 </template>
 
 <script>
@@ -123,7 +130,8 @@ export default {
   data() {
     return {
       json: week[this.week] || {},
-      overlay: {}
+      overlay: {},
+      active: false
     }
   },
   methods: {
